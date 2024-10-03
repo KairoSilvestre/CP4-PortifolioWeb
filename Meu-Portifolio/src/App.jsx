58e66b1a042c9aa3747d3a67505e5033bf5686be
KairoSilvestre <kairodasilva.silvestre@gmail.com>
2024-10-02 21:45:34 -0300

    Configurado para programar

---
+++ Meu-Portifolio/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold'>Olá mundo</h1>
+      {/* Tailwind Funcionando */}
+    </>
+  )
+}
+
+export default App
